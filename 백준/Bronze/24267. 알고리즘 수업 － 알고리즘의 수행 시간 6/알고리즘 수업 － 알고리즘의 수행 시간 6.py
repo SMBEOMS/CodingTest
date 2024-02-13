b5d80822059136c90fc0d8d571515acc63c43a68
JEONG BEOMSEOK <118713480+SMBEOMS@users.noreply.github.com>
2024-02-13 17:20:56 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.py
@@ -0,0 +1,11 @@
+# 입력의 크기 n을 입력받음
+n = int(input())
+
+# 코드1의 수행 횟수 계산 (nC3 공식 사용)
+execution_count = n * (n - 1) * (n - 2) // 6
+
+# 코드1의 수행 횟수 출력
+print(execution_count)
+
+# 코드1의 수행 횟수를 다항식으로 나타냈을 때 최고차항의 차수 출력
+print(3)
